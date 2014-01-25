415741c6521a8e256c65fac3212e46a16913d917
rogetfan <huxuehan1991@gmail.com>
2014-01-25 22:27:14 +0800

    Signed-off-by: rogetfan <huxuehan1991@gmail.com>

--- src/org/simpleim/server/server/ServerHandler.java
+++ src/org/simpleim/server/server/ServerHandler.java
@@ -12,46 +12,44 @@ import org.simpleim.common.message.*;
 import org.simpleim.server.database.DataBase;
 import org.simpleim.server.util.AccountGenerator;
 
+import com.lambdaworks.crypto.SCryptUtil;
+
 public class ServerHandler extends ChannelHandlerAdapter {
 
-	private static final Logger logger = Logger.getLogger(ServerHandler.class.getName());
-	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		boolean keepAlive = false;
-		Response response = null;
-		String id;
-	    String password;
-		if(msg instanceof NewAccountRequest) {
-			keepAlive = false;
-			id=AccountGenerator.nextId();
-			password=AccountGenerator.generatePassword();
-			response = new NewAccountOkResponse()
-						.setId(id)
-						.setPassword(password);
-			DataBase.InsertNumberRow(id,password);
-		} else if(msg instanceof LoginRequest ){
-			keepAlive=true;
-			id=((LoginRequest) msg).getId();
-			password=((LoginRequest) msg).getPassword();
-			
-		}else {
-			keepAlive = false;
-			response = new FailureResponse();
-		}
-		ChannelFuture f = ctx.write(response);
-		if(!keepAlive)
-			f.addListener(ChannelFutureListener.CLOSE);
-	}
+    private static final Logger logger = Logger.getLogger(ServerHandler.class.getName());
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            boolean closeNow = true;
+            Response response = null;
+            String id;
+            String password;
+            if(msg instanceof NewAccountRequest) {
+                    closeNow = true;
+                    id=AccountGenerator.nextId();
+                    password=AccountGenerator.generatePassword();
+                    response = new NewAccountOkResponse()
+                                            .setId(id)
+                                            .setPassword(password);
+                    String hashedPassword = SCryptUtil.scrypt(password, 1 << 15, 8, 1);
+                    DataBase.InsertNumberRow(id, hashedPassword);
+            } else {
+                    closeNow = true;
+                    response = new FailureResponse();
+            }
+            ChannelFuture f = ctx.write(response);
+            if(closeNow)
+                    f.addListener(ChannelFutureListener.CLOSE);
+    }
 
-	@Override
-	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-		ctx.flush();
-	}
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.flush();
+    }
 
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-		// Close the connection when an exception is raised.
-		logger.log(Level.WARNING, "Unexpected exception from downstream.", cause);
-		ctx.close();
-	}
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            // Close the connection when an exception is raised.
+            logger.log(Level.WARNING, "Unexpected exception from downstream.", cause);
+            ctx.close();
+    }
 }
